992ecf5082b70af37b683cb51bbdd830b18cea23
丸山祐矢 <rsa14759@gmail.com>
2019-06-03 00:22:24 +0900

    update(改修)
    メッセージ閲覧画面以外でメッセージの監視を行わないように修正

--- app/assets/javascripts/matches.js
+++ app/assets/javascripts/matches.js
@@ -1,26 +1,28 @@
-$(function () {
-	$(window).scroll(function() {
-		$("#new-message").offset({
-			top: $(this).scrollTop() + $(".app-title").height() + $("#new-message").height(),
-			left: $(".container").offset().left
+if(location.pathname == "/matches/show") {
+	$(function () {
+		$(window).scroll(function() {
+			$("#new-message").offset({
+				top: $(this).scrollTop() + $(".app-title").height() + $("#new-message").height(),
+				left: $(".container").offset().left
+			});
 		});
-	});
 
-	$("#text").keyup(function () {
-		var count = $(this).val().replace(/(\r\n|\r|\n)/g, "\r\n").length;
-		$("div.count.nav.justify-content-end").text(count + " / 255文字");
-	});
-
-	setInterval(function() {
-		$.ajax({
-			url: "/messages/count_message",
-			type: "POST",
-			data: {
-				message_id: $("p.id:last").text(),
-				matching_id: $("p#matching-id").text()
-			},
-			success: function () {
-			}
+		$("#text").keyup(function () {
+			var count = $(this).val().replace(/(\r\n|\r|\n)/g, "\r\n").length;
+			$("div.count.nav.justify-content-end").text(count + " / 255文字");
 		});
-	}, 10000);
-});
+
+		setInterval(function() {
+			$.ajax({
+				url: "/messages/count_message",
+				type: "POST",
+				data: {
+					message_id: $("p.id:last").text(),
+					matching_id: $("p#matching-id").text()
+				},
+				success: function () {
+				}
+			});
+		}, 10000);
+	});
+}
